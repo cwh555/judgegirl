bab6bebfc2f2e4c7f74fcad707f09564b49d01c3
cwh555 <bergerchen719@gmail.com>
2024-10-02 21:16:46 +0800

    AC

--- 2015/50000/50000.c
+++ 2015/50000/50000.c
@@ -6,10 +6,8 @@ int main(){
     int k;
     scanf("%d", &k);
  
-    int len_now = 0;
-    int len_before = 0;
-    int alternate_len_now = 0;
-    int max_len = 0;
+    int len_now = 0, len_before = 0;
+    int alternate_len_now = 0, max_len = 0;
  
     int input = 1;
     int before = 1;
@@ -18,8 +16,6 @@ int main(){
     before = input;
     len_now = 1;
     while(scanf("%d", &input) != EOF && input != 0){
- 
- 
         if(input * before > 0){
             //沒變號
             len_now++;
@@ -29,10 +25,6 @@ int main(){
             len_before = len_now;
             len_now = 1;
         }
- 
-        #ifdef debug
-        printf("input %d before %d len_now %d len_before %d\n", input, before, len_now, len_before);
-        #endif
  
         //第一次更新alternating_len_now
         if(alternate_len_now == 0 && before > 0 && input < 0 && len_before >= k)
