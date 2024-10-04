e01cca9844785a234f23e685613015568aa329de
cwh555 <bergerchen719@gmail.com>
2024-10-04 16:23:46 +0800

    AC three layer

--- 2015/50008/50008.c
+++ 2015/50008/50008.c
@@ -2,13 +2,18 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void processSetMatrix(int **matrix){
-    while(*matrix != NULL){
-        int *array = *matrix;
-        int index = 0;
-        while(array[index] != 0)
-            printf("%d ", array[index++]);
+void processSetMatrix(int ***ptr){
+    while(*ptr != NULL){
+        int **matrix = *ptr;
+        while(*matrix != NULL){
+            int *array = *matrix;
+            int index = 0;
+            while(array[index] != 0)
+                printf("%d ", array[index++]);
 
-        matrix++;
+            matrix++;
+        }
+        ptr++;
     }
+
 }
