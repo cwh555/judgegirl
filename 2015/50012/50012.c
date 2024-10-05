fc10b674e8c30c242c0e29c24598a9fa3cc70c55
cwh555 <bergerchen719@gmail.com>
2024-10-05 17:19:00 +0800

    program printBlock

---
+++ 2015/50012/50012.c
@@ -0,0 +1,37 @@
+#include <stdio.h>                                                                              
+#include "blockmover.h"
+ 
+void printBlock(unsigned long long int *block) {
+    long long int value = *block;
+    int index = 1;
+    while(index <= 64){
+        printf("%lld%c", value & 1LL, "\0\n"[index % 8 == 0]);
+        index++;
+        value >>= 1LL;
+    }
+    
+}
+ 
+void initialize(unsigned long long int *block, int row, int column, int size) {
+    // your implementation here
+}
+ 
+int moveLeft(unsigned long long int *block) {
+    // your implementation here    
+    return 0; // You may change this return value.
+}
+ 
+int moveRight(unsigned long long int *block) {
+    // your implementation here    
+    return 0; // You may change this return value.
+}
+ 
+int moveUp(unsigned long long int *block) {
+    // your implementation here    
+    return 0; // You may change this return value.
+}
+ 
+int moveDown(unsigned long long int *block) {
+    // your implementation here    
+    return 0; // You may change this return value.
+}
