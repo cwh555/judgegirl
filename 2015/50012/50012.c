1376f64ff617883f173cbd9de14ffca77c7c85e3
cwh555 <bergerchen719@gmail.com>
2024-10-05 17:41:33 +0800

    program initialize

--- 2015/50012/50012.c
+++ 2015/50012/50012.c
@@ -13,9 +13,23 @@ void printBlock(unsigned long long int *block) {
 }
  
 void initialize(unsigned long long int *block, int row, int column, int size) {
-    // your implementation here
+    unsigned long long int value = 0;
+    for(int i = 0; i < size; i++){
+        //把之前的值推到下一row
+        value <<= 8;
+        //在末位放上size個1 推到column的位置
+        value |= (((1LL << size) - 1LL) << column);
+    }
+
+    //推到row
+    for(int i = 0; i < row; i++)
+        value <<= 8;
+
+    *block = value;
+
+    return ;
 }
- 
+
 int moveLeft(unsigned long long int *block) {
     *block >>= 1LL;     
     return 0; // You may change this return value.
