0cd17145a9de136401c7833bc94f405ee82ad7af
cwh555 <bergerchen719@gmail.com>
2024-10-05 18:15:46 +0800

    debug
    85%AC 15%TLE

--- 2015/50012/50012.c
+++ 2015/50012/50012.c
@@ -2,14 +2,15 @@
 #include "blockmover.h"
  
 void printBlock(unsigned long long int *block) {
-    long long int value = *block;
-    int index = 1;
-    while(index <= 64){
-        printf("%lld%c", value & 1LL, "\0\n"[index % 8 == 0]);
-        index++;
-        value >>= 1LL;
+    unsigned long long value = *block;
+    for(int i = 0; i < 8; i++){
+        for(int j = 0; j < 8; j++){
+            printf("%d", (value & 1ULL ? 1 : 0));
+            value >>= 1;
+        }
+        printf("\n");
     }
-    
+
 }
  
 void initialize(unsigned long long int *block, int row, int column, int size) {
@@ -18,7 +19,7 @@ void initialize(unsigned long long int *block, int row, int column, int size) {
         //把之前的值推到下一row
         value <<= 8;
         //在末位放上size個1 推到column的位置
-        value |= (((1LL << size) - 1LL) << column);
+        value |= (((1ULL << size) - 1ULL) << column);
     }
 
     //推到row
@@ -36,13 +37,13 @@ int moveLeft(unsigned long long int *block) {
     unsigned long long left_bound = 0;
     for(int i = 0; i < 8; i++){
         left_bound <<= 8;
-        left_bound |= 1LL;
+        left_bound |= 1ULL;
     }
 
     if(left_bound & *block)
         return 0;
     else{
-        *block >>= 1LL;     
+        *block >>= 1;     
         return 1; 
     }
 }
@@ -52,38 +53,38 @@ int moveRight(unsigned long long int *block) {
     unsigned long long right_bound = 0;
     for(int i = 0; i < 8; i++){
         right_bound <<= 8;
-        right_bound |= (1LL << 7);
+        right_bound |= (1ULL << 7);
     }
 
     if(right_bound & *block)
         return 0;
     else{
-        *block <<= 1LL;  
+        *block <<= 1;  
         return 1;
     }
 }
  
 int moveUp(unsigned long long int *block) {
     //check up bound
-    unsigned long long upper_bound = (1LL << 8) - 1;
+    unsigned long long upper_bound = (1ULL << 8) - 1ULL;
     
     if(upper_bound & *block)
         return 0;
     else{
-        *block >>= 8LL;
+        *block >>= 8;
         return 1;
     }
 }
  
 int moveDown(unsigned long long int *block) {
     //check down bound
-    unsigned long long down_bound = (1LL << 8) - 1;
+    unsigned long long down_bound = (1ULL << 8) - 1ULL;
     down_bound <<= 56;
 
     if(down_bound & *block)
         return 0;
     else{
-        *block <<= 8LL;  
+        *block <<= 8;  
         return 1;
     }
 }
