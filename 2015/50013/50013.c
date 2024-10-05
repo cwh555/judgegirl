aa365f41b5715195e38deeba6814cc774cd88d98
cwh555 <bergerchen719@gmail.com>
2024-10-05 19:16:27 +0800

    debug logical error

--- 2015/50013/50013.c
+++ 2015/50013/50013.c
@@ -1,4 +1,4 @@
-#include "bingo.h"
+#include <stdio.h>
 #include <stdbool.h>
 
 
@@ -6,48 +6,66 @@ int bingo(const unsigned long long int *board, int *rowColumn){
     int find = 0;
     unsigned long long int checker = 0;
     //檢查row
-    checker = (1ULL << 8 ) - 1ULL;
+    checker = ((1ULL << 8 ) - 1ULL) << 56;
     for(int i = 0; i < 8 && find != 1; i++){
         if((*board & checker) == checker){
             find = 1;
-            *rowColumn = 7 - i;
+            *rowColumn = i;
         }
-        checker <<= 8;
+        checker >>= 8;
     }
 
     //檢查column
-    checker = (0x0101010101010101ULL);
+    checker = (0x8080808080808080ULL);
     bool find_column = false;
     for(int i = 0; i < 8 && !find_column; i++){
         if((*board & checker) == checker){
             if(find == 0){
-                find = 1;
-                *rowColumn = 7 - i;
+                find = 2;
+                *rowColumn = i;
             }
-            else
-                *rowColumn = 0;
+            else if(i < *rowColumn)
+                *rowColumn = i;
             find_column = true;
         }
+        checker >>= 1;
     }
 
     //檢查左上對角線
+    bool leftup = false;
     checker = (0x8040201008040201ULL);
     if((checker & *board) == checker){
         if(find == 0)
             find = 3;
-        *rowColumn = 0;
+        leftup = true;
     }
 
     //檢查右上對角線
     checker = (0x0102040810204080ULL);
+    bool rightup = false;
     if((checker & *board) == checker){
-        if(find == 0){
+        if(find == 0)
             find = 3;
+        rightup = true;
+    }
+
+    if(leftup || rightup){
+        *rowColumn = 0;
+        if(!leftup && rightup)
             *rowColumn = 1;
-        }
-        else
-            *rowColumn = 0;
     }
 
     return find;
+}
+
+
+int main(void)
+{
+    unsigned long long int board;
+    int res = 0, rowColumn = 0;
+    scanf("%llu", &board);
+    res = bingo(&board, &rowColumn);
+    if(res == 0) printf("no\n");
+    else printf("%d %d\n", res, rowColumn);
+    return 0;
 }
