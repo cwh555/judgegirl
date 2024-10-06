4588e9d83d667f53b3c52f84b3a2b13d927501cb
cwh555 <bergerchen719@gmail.com>
2024-10-06 17:46:21 +0800

    AC
    debug invalid condition

--- 2015/50017/50017.c
+++ 2015/50017/50017.c
@@ -3,7 +3,6 @@
 #include "expression.h"
 #include <ctype.h>
 #include <stdbool.h>
-#include <assert.h>
 
 
 enum {
@@ -27,13 +26,13 @@ int caculate(int opd1, int opr, int opd2){
     }
 }
 
-int expression_eval(char *string, int *length){
-    if(isdigit(string[0])){
+int expression_eval(char *string, int *length, bool first, bool in){
+    if(isdigit(string[0]) && (in || string[1] == '\0')){
         *length = 1;
         return (int)(string[0] - '0');
     }
     else if(string[0] == '-'){
-        int value =  expression_eval(string + 1, length);
+        int value =  expression_eval(string + 1, length, first, in);
         
         if(value == FALSENUM)
             return FALSENUM;
@@ -41,28 +40,36 @@ int expression_eval(char *string, int *length){
         (*length) ++;
         return -value;
     }
-    else if(string[0] == '('){
+    else if(string[0] == '(' && (in || first)){
         int len;
-        int value1 =  expression_eval(string + 1, &len);
-        int opr = string[len + 1];
-        *length = len + 1;
-        
+        *length = 1;
+        int value1 =  expression_eval(string + (*length), &len, false, true);
+        *length += len;
+        int opr = string[*length];
+        (*length)++;    //經過運算子
+
         #ifdef debug
             printf("value1 : %d\n", value1);
             printf("opr %d\n", opr);
             printf("length after value1: %d\n", *length);
         #endif
 
-        int value2 = expression_eval(string + len + 2, &len);
-        *length += len + 1;
+        int value2 = expression_eval(string + (*length), &len, false, true);
+        (*length) += len;
 
         #ifdef debug
             printf("value2: %d\n", value2);
+            printf("length after value2: %d\n", *length);
         #endif
 
         if(string[*length] != ')' || value1 == FALSENUM || value2 == FALSENUM)
             return FALSENUM;
+        (*length)++;    //經過後括號
 
+        //後括號後必須結束
+        if(!in && string[*length] != '\0')
+            return FALSENUM;
+            
         return caculate(value1, opr, value2);
     }
     else
@@ -72,7 +79,9 @@ int expression_eval(char *string, int *length){
 
 }
 
+
 int expression(char *string){
     int len;
-    return expression_eval(string, &len);
+    return expression_eval(string, &len, true, false);
 }
+ 
