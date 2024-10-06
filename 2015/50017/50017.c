970a66160363df4e7b9875e93e0847710cc01641
cwh555 <bergerchen719@gmail.com>
2024-10-06 14:59:20 +0800

    program condition 3, 4

--- 2015/50017/50017.c
+++ 2015/50017/50017.c
@@ -1,12 +1,67 @@
 #include "expression.h"
 #include <ctype.h>
 #include <stdbool.h>
+#include <assert.h>
 
-int expression(char *string){
-    if(string[1] == '\0')
+
+enum {
+    ADD = '+', MINUS = '-', MUL = '*', DIV = '/'
+};
+
+int caculate(int opd1, int opr, int opd2){
+    switch(opr){
+        case '+':
+            return opd1 + opd2;
+        case '-':
+            return opd1 - opd2;
+        case '*':
+            return opd1 * opd2;
+        case '/':
+            return opd1 / opd2;
+    }
+}
+
+int expression_eval(char *string, int *length){
+    if(isdigit(string[0])){
+        *length = 1;
         return (int)(string[0] - '0');
-    else if(string[0] == '-')
-        return -expression(string + 1);
+    }
+    else if(string[0] == '-'){
+        int value =  -expression_eval(string + 1, length);
+        (*length) ++;
+        return value;
+    }
+    else if(string[0] == '('){
+        int len;
+        int value1 =  expression_eval(string + 1, &len);
+        int opr = string[len + 1];
+        *length = len + 1;
+        
+        #ifdef debug
+            printf("value1 : %d\n", value1);
+            printf("opr %d\n", opr);
+            printf("length after value1: %d\n", *length);
+        #endif
+
+        int value2 = expression_eval(string + len + 2, &len);
+        *length += len + 1;
+
+        #ifdef debug
+            printf("value2: %d\n", value2);
+        #endif
+
+        assert(string[*length] == ')');
+        return caculate(value1, opr, value2);
+    }
+    else if(string[0] == ')'){
+        return 0;
+    }
+    
+
 
 }
 
+int expression(char *string){
+    int len;
+    return expression_eval(string, &len);
+}
