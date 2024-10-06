d37776c54b91384d3ee02cae86657cc65373cf25
cwh555 <bergerchen719@gmail.com>
2024-10-06 14:23:25 +0800

    change to recursion form

--- 2015/50017/50017.c
+++ 2015/50017/50017.c
@@ -2,29 +2,11 @@
 #include <ctype.h>
 #include <stdbool.h>
 
-enum {
-    ADD, MINUS, MUL, DIV
-};
-
-//length為需要計算的長度
-int expressionEval(char *string, int *length){
-
-}
-
 int expression(char *string){
-    int len = strlen(string);
+    if(string[1] == '\0')
+        return (int)(string[0] - '0');
+    else if(string[0] == '-')
+        return -expression(string + 1);
 
-    int value = 0;
-    bool negative = false;
-    for(int i = 0; i < len; i++){
-        if(isdigit(string[i])){
-            value = (string[i] - '0') * (negative ? -1 : 1);
-            negative = false;
-        }
-        else if(string[i] == '-')
-            negative = !negative;
-    }
-
-    return value;
 }
 
