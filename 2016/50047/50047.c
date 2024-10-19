646a00ea16cc576a11cd19fb7554018a59387bcb
cwh555 <bergerchen719@gmail.com>
2024-10-19 16:46:36 +0800

    AC
    upgrade the method of checking boundary

--- 2016/50047/50047.c
+++ 2016/50047/50047.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
  
-int neighbor_healthy(int grid[50][50], int row, int column){
+int neighbor_healthy(int grid[52][52], int row, int column){
     int dir_x[8] = {-1, -1, -1, 0, 0, 1, 1, 1};
     int dir_y[8] = {-1, 0, 1, -1, 1, -1, 0, 1};
  
@@ -11,50 +11,47 @@ int neighbor_healthy(int grid[50][50], int row, int column){
         int neighbor_row = row + dir_x[dir];
         int neighbor_column = column + dir_y[dir];
  
-        //check the bound
-        if(neighbor_column < 0 || neighbor_column >= 50 || neighbor_row < 0 || neighbor_row >= 50)
-            continue;
+ 
  
         //check status
-        if(grid[neighbor_row][neighbor_column] == 2)
-            healthy ++;
+        healthy += (grid[neighbor_row][neighbor_column] / 2); 
     }
  
     return healthy;
 }
  
 void game_of_cell(int grid[50][50],int outcome[50][50],int N){
-    int record[50][50];
     int status_change[3][9] = {
-        {0, 0, 0, 2, 0, 0, 0, 0, 0, 0},
-        {1, 1, 2, 1, 1, 1, 1, 1, 1, 1},
-        {0, 0, 2, 2, 1, 1, 1, 1, 1, 1}
+        {0, 0, 0, 2, 0, 0, 0, 0, 0},
+        {1, 1, 2, 1, 1, 1, 1, 1, 1},
+        {0, 0, 2, 2, 1, 1, 1, 1, 1}
     };
  
     //copy data
-    for(int i = 0; i < 50; i++)
-        for(int j = 0; j < 50; j++)
-            record[i][j] = grid[i][j];
+    int bound_record[52][52] = {0};
+    for(int i = 1; i <= 50; i++)
+        for(int j = 1; j <= 50; j++)
+            bound_record[i][j] = grid[i - 1][j - 1];
  
     for(int time = 0; time < N; time++){
         //record life num 
-        int life[50][50] = {0};
-        for(int row = 0; row < 50; row++)
-            for(int column = 0; column < 50; column++)
-                life[row][column] = neighbor_healthy(record, row, column);
+        int life[52][52] = {0};
+        for(int row = 1; row <= 50; row++)
+            for(int column = 1; column <= 50; column++)
+                life[row][column] = neighbor_healthy(bound_record, row, column);
  
-        for(int i = 0; i < 50; i++)
-            for(int j = 0; j < 50; j++)
-                record[i][j] = status_change[record[i][j]][life[i][j]];
+        for(int i = 1; i <= 50; i++)
+            for(int j = 1; j <= 50; j++)
+                bound_record[i][j] = status_change[bound_record[i][j]][life[i][j]];
  
  
  
     }
  
     //copy data 
-    for(int i = 0; i < 50; i++)
-        for(int j = 0; j < 50; j++)
-            outcome[i][j] = record[i][j];
+    for(int i = 1; i <= 50; i++)
+        for(int j = 1; j <= 50; j++)
+            outcome[i - 1][j - 1] = bound_record[i][j];
  
     return;
  
