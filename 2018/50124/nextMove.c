dcb1f58e2c6265d841ffb96ce00e2e2ff4089149
cwh555 <bergerchen719@gmail.com>
2024-11-13 16:40:26 +0800

    program nextMove

---
+++ 2018/50124/nextMove.c
@@ -0,0 +1,27 @@
+#include "validMoveNum.h"
+
+int nextMove(int r, int c, int n, int visited[100][100]){
+    if(validMoveNum(r, c, n, visited) <= 0)
+        return -1;
+    
+    int move[8][2] = {{-2,1},{-1,2},{1,2},{2,1},
+                        {2,-1},{1,-2},{-1,-2},{-2,-1}};
+
+    int min = 10;
+    int min_index = -1;
+
+    for(int i = 0; i < 8; i++){
+        int num = validMoveNum(r + move[i][0], c + move[i][1], n, visited);
+
+        if(num == -1)
+            continue;
+        
+        if(min > num){
+            min = num;
+            min_index = i;
+        }
+    }
+
+    return min_index;
+
+}
