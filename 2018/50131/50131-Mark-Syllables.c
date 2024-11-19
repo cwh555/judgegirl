7e9975861bd928d56882174d9489804b7f27d85a
cwh555 <bergerchen719@gmail.com>
2024-11-19 20:05:42 +0800

    AC the problem before: if you printf("\0") then '\0' will be written into the file however, if you printf(string) then '\0' won't be written into the file

--- 2018/50131/50131-Mark-Syllables.c
+++ 2018/50131/50131-Mark-Syllables.c
@@ -4,7 +4,37 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
- 
+
+void print_slice(char *slice[], int slice_num, char vowel[]){
+    if(slice_num <= 0)
+        return;
+
+    for(int i = 0; i < slice_num; i++){
+        printf("%s", slice[i]);
+        if(vowel[i] != '\0')
+            printf("%c", vowel[i]);
+    }
+
+    return;
+}
+
+void print_plus(char *slice[], int slice_num, char vowel[]){
+    if(slice_num <= 0)
+        return;
+
+    for(int i = 0; i < slice_num; i++){
+        int len = strlen(slice[i]);
+
+        for(int j = 0; j < len; j++)
+            printf("+");
+        
+        if(vowel[i] != '\0')
+            printf("+");
+    }
+
+    return;
+}
+
 int main(){
     char string[MAXLEN];
     int i, j;
@@ -12,75 +42,47 @@ int main(){
         char *slice[MAXLEN];
         int slice_num = 0;
         char vowel[MAXLEN];
- 
+        
         slice[0] = string;
- 
+
         char *find = strpbrk(slice[slice_num], "aeiou");
         while(find != NULL){
             vowel[slice_num++] = *find;
- 
+
             *find = '\0';
             slice[slice_num] = find + 1;
             find = strpbrk(slice[slice_num], "aeiou");
         }
- 
+        
         //last
         if(*slice[slice_num] != '\0'){
             vowel[slice_num] = '\0';
             slice_num++;
         }
-        int len;
- 
+
         //print i
-        for(int count = 0; count < i; count++){
-            len = strlen(slice[count]) + (vowel[count] != '\0' ? 1 : 0);
-            for(int t = 0; t < len; t++)
-                printf("+");
-        }
-        printf("%s%c", slice[i], vowel[i]);
-        for(int count = i + 1; count < slice_num; count++){
-            len = strlen(slice[count]) + (vowel[count] != '\0' ? 1 : 0);
-            for(int t = 0; t < len; t++)
-                printf("+");
-        }
+        print_plus(slice, i, vowel);
+        print_slice(slice + i, 1, vowel + i);
+        print_plus(slice + i + 1, slice_num - i - 1, vowel + i + 1);
         printf("\n");
- 
- 
+
         //print j
-        for(int count = 0; count < j; count++){
-            len = strlen(slice[count]) + (vowel[count] != '\0' ? 1 : 0);
-            for(int t = 0; t < len; t++)
-                printf("+");
-        }
-        printf("%s%c", slice[j], vowel[j]);
-        for(int count = j + 1; count < slice_num; count++){
-            len = strlen(slice[count]) + (vowel[count] != '\0' ? 1 : 0);
-            for(int t = 0; t < len; t++)
-                printf("+");
-        }
+        print_plus(slice, j, vowel);
+        print_slice(slice + j, 1, vowel + j);
+        print_plus(slice + j + 1, slice_num - j - 1, vowel + j + 1);
         printf("\n");
- 
- 
+
         //print rest
         int min = (i < j ? i : j);
         int max = (i > j ? i : j);
- 
-        for(int count = 0; count < min; count++)
-            printf("%s%c", slice[count], vowel[count]);
-        len = strlen(slice[min]) + (vowel[min] != '\0' ? 1 : 0);
-        for(int count = 0; count < len; count++)
-            printf("+");
-        for(int count = min + 1; count < max; count++)
-            printf("%s%c", slice[count], vowel[count]);
-        len = strlen(slice[max]) + (vowel[max] != '\0' ? 1 : 0);
-        for(int count = 0; count < len; count++)
-            printf("+");
-        for(int count = max + 1; count < slice_num; count++)
-            printf("%s%c", slice[count], vowel[count]);
- 
+
+        print_slice(slice, min, vowel);
+        print_plus(slice + min, 1, vowel + min);
+        print_slice(slice + min + 1, max - min - 1, vowel + min + 1);
+        print_plus(slice + max, 1, vowel + max);
+        print_slice(slice + max + 1, slice_num - max - 1, vowel + max + 1);
         printf("\n");
- 
     }
- 
- 
+
+
 }
