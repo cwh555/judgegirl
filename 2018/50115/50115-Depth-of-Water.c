509a0366d8ab06060751299d1db298d63ea3135d
cwh555 <bergerchen719@gmail.com>
2024-11-23 10:43:53 +0800

    fix input problem

--- 2018/50115/50115-Depth-of-Water.c
+++ 2018/50115/50115-Depth-of-Water.c
@@ -2,7 +2,7 @@
 
 int main(){
     int a, b, h, c, d;
-    scanf("%d %d %d %d %d", &a, &b, &c, &h, &d);
+    scanf("%d %d %d %d %d", &a, &b, &h, &c, &d);
     int ans = (a * b * h) / (a * b - c * d);
 
     printf("%d\n", ans);
