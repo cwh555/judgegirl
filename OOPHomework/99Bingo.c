e7ddb5b4c5335e58d3ed42bbc72b2620814309bc
cwh555 <bergerchen719@gmail.com>
2025-03-23 16:09:39 +0800

    board initialize function

--- OOPHomework/99Bingo.c
+++ OOPHomework/99Bingo.c
@@ -9,35 +9,38 @@ typedef struct board{
     int* column_pick;   //紀錄每行有幾個已經被選擇
     int diagonal[2];   //紀錄右下、左下對角線
 }Board;
- 
+
+Board *initBoard(const int bingoSize){
+    Board *board = (Board *)malloc(sizeof(Board));
+    scanf("%s", board->name);
+
+    board->row_pick = (int*)calloc(bingoSize, sizeof(int));
+    board->column_pick = (int*)calloc(bingoSize, sizeof(int));
+    board->diagonal[0] = 0;
+    board->diagonal[1] = 0;
+    board->data = (int(*)[2])malloc(sizeof(int[2]) * bingoSize * bingoSize);
+
+    for(int bingoRow = 0; bingoRow < bingoSize; bingoRow++){
+        for(int bingoColumn = 0; bingoColumn < bingoSize; bingoColumn++){
+            int temp;
+            scanf("%d", &temp);
+            board->data[temp - 1][0] = bingoRow;
+            board->data[temp - 1][1] = bingoColumn;
+        }
+    }
+    return board;
+}
+
 bool get_number(Board *data, int target, int bingo_size);
  
 int main(){
     int people_num, bingo_size;
     scanf("%d%d", &people_num, &bingo_size);
- 
-    Board* record = (Board*)malloc(sizeof(Board) * people_num);
-    for(int i = 0; i < people_num; i++){
-        scanf("%s", record[i].name);
- 
-        record[i].row_pick = (int*)calloc(bingo_size, sizeof(int));
-        record[i].column_pick = (int*)calloc(bingo_size, sizeof(int));
-        record[i].diagonal[0] = 0;
-        record[i].diagonal[1] = 0;
- 
-        record[i].data = (int(*)[2])malloc(sizeof(int[2]) * bingo_size * bingo_size);
- 
-        for(int bingo_row = 0; bingo_row < bingo_size; bingo_row++){
-            for(int bingo_column = 0; bingo_column < bingo_size; bingo_column++){
-                int temp;
-                scanf("%d", &temp);
-                record[i].data[temp - 1][0] = bingo_row;
-                record[i].data[temp - 1][1] = bingo_column;
-            }
-        }
- 
-    }
- 
+    
+    Board **record = (Board *)malloc(sizeof(Board) * people_num);
+    for(int i = 0; i < people_num; i++)
+        record[i] = initBoard(bingo_size);
+    
     bool end = false;
  
     for(int i = 0; i < bingo_size * bingo_size; i++){
@@ -48,19 +51,18 @@ int main(){
             continue;
  
         for(int people_index = 0;people_index < people_num; people_index++){
-            if(get_number(&record[people_index], input - 1, bingo_size)){
+            if(get_number(record[people_index], input - 1, bingo_size)){
                 if(!end)
                     printf("%d", input);
-                printf(" %s", record[people_index].name);
+                printf(" %s", record[people_index]->name);
  
                 end = true;
             }
         }
     }
  
-    for(int i = 0; i < people_num; i++){
-        free(record[i].data);
-    }
+    for(int i = 0; i < people_num; i++)
+        free(record[i]->data);
     free(record);
  
     return 0;
