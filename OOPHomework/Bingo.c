d1c089d350113603e331a00d8041d0a448f8fc11
cwh555 <bergerchen719@gmail.com>
2025-04-03 21:29:11 +0800

    move Board to another file

--- OOPHomework/Bingo.c
+++ OOPHomework/Bingo.c
@@ -25,28 +25,6 @@ void endGame(Game *game){
     free(game);
 }
 
-Board *initBoard(const int bingoSize){
-    Board *board = (Board *)malloc(sizeof(Board));
-    scanf("%s", board->name);
-
-    board->boardSize = bingoSize;
-    board->row_pick = (int*)calloc(bingoSize, sizeof(int));
-    board->column_pick = (int*)calloc(bingoSize, sizeof(int));
-    board->diagonal[0] = 0;
-    board->diagonal[1] = 0;
-    board->data = (Position *)malloc(sizeof(Position) * bingoSize * bingoSize);
-
-    for(int bingoRow = 0; bingoRow < bingoSize; bingoRow++){
-        for(int bingoColumn = 0; bingoColumn < bingoSize; bingoColumn++){
-            int temp;
-            scanf("%d", &temp);
-            board->data[temp - 1].x = bingoRow;
-            board->data[temp - 1].y = bingoColumn;
-        }
-    }
-    return board;
-}
-
 bool playOneBingo(Game *game, const int choice, int peopleIndex){
     if(get_number(game->record[peopleIndex], choice - 1)){
         if(!game->gameEnd)
@@ -69,29 +47,4 @@ void OneRound(Game *game){
     for(int people_index = 0; people_index < game->peopleNum; people_index++)
         game->gameEnd |= playOneBingo(game, input, people_index);
     return;
-}
-
-bool win(Board *people, Position numPlace){
-    return (people->row_pick[numPlace.x] == people->boardSize) ||
-            (people->column_pick[numPlace.y] == people->boardSize) ||
-            (numPlace.x == numPlace.y && people->diagonal[0] == people->boardSize) ||
-            (numPlace.x + numPlace.y == people->boardSize - 1 && people->diagonal[1] == people->boardSize);
-}
- 
-bool get_number(Board* people, int target){
-    Position numPlace = people->data[target];
-    //get the target 
-    people->column_pick[numPlace.y]++;
-    people->row_pick[numPlace.x]++;
-    if(numPlace.x == numPlace.y)
-        people->diagonal[0]++;
-    if(numPlace.x + numPlace.y == people->boardSize - 1)
-        people->diagonal[1]++;
-
-    return win(people, numPlace);
-}
-
-void destroyBoard(Board *board){
-    free(board->data);
-    free(board);
 }
--- OOPHomework/Bingo.h
+++ OOPHomework/Bingo.h
@@ -3,19 +3,7 @@
 
 #include <stdbool.h>
 #include <stdlib.h>
-
-typedef struct position{
-    int x, y;
-}Position;
-
-typedef struct board{
-    char name[100];
-    Position *data; //紀錄index所在的位置
-    int* row_pick; //紀錄每列有幾個已經被選擇
-    int* column_pick;   //紀錄每行有幾個已經被選擇
-    int diagonal[2];   //紀錄右下、左下對角線
-    int boardSize;
-}Board;
+#include "Board.h"
 
 typedef struct game{
     int peopleNum, bingoSize;
@@ -26,12 +14,7 @@ typedef struct game{
 Game *initGame();
 void playGame(Game *game);
 void endGame(Game *game);
-
-Board *initBoard(const int bingoSize);
-bool get_number(Board *data, int target);
 void OneRound(Game *game);
 bool playOneBingo(Game *game, const int choice, int peopleIndex);
-
-void destroyBoard(Board *board);
 
 #endif
